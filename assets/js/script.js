4913563db8a2baed23204cd524a3909dd72fe3ad
SinitYT <sinityonas@outlook.com>
2022-11-11 14:36:54 +0000

    wire up the Run Checks button to post a request and display the errors in a modal after checking

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,46 @@ const API_KEY = "IUDJHvqI9u3fhHWcnda0WvwoeuM";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resutlsModal = new bootstrap.Modal(document.getElementById('resultsModal'));
 
-//  LETS WIRE UP OUR BUTTON
+//  LETS WIRE UP OUR Check Key  BUTTON
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+// Wiring Up Run checks button
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+                    "Authorization": API_KEY,
+                },
+        body: form,
+        })
+    const data = await response.json();
+
+    if (response.ok){
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+function displayErrors(data) {
+    heading = `JSHint results for ${data.file}`
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors=<span class="error_count">${data.total_errors}</span></div>`
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}:</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+    document.getElementById("resultsModalTitle").innerHTML = heading;
+    document.getElementById("results-content").innerHTML =  results;
+    resutlsModal.show()
+
+
+}
+
 
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
@@ -23,3 +61,6 @@ function displayStatus(data){
     document.getElementById("results-content").innerHTML =  `Your key is valid until <br> ${data.expiry}`;
     resutlsModal.show()
 }
+
+
+
