37e8f563d3fff8289e8bd1ce422413b29fa73684
SinitYT <sinityonas@outlook.com>
2022-11-11 12:34:23 +0000

    Wire up the Check Key button to retrieve data with get request and display returned data in a modal with basic error handling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
-const API_key = "IUDJHvqI9u3fhHWcnda0WvwoeuM";
-const API_URL = "https://ci-jshint.herokuapp.com";
+const API_KEY = "IUDJHvqI9u3fhHWcnda0WvwoeuM";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resutlsModal = new bootstrap.Modal(document.getElementById('resultsModal'));
 
+//  LETS WIRE UP OUR BUTTON
+document.getElementById("status").addEventListener("click", e => getStatus(e));
 
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+    const response = await fetch(queryString);
+    const data = await response.json(); // Using await pauses the execution of its surrounding async function until the promise is settled (that is, fulfilled or rejected).
+
+    if (response.ok) {
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
+    }
+    
+}
+
+function displayStatus(data){
+    document.getElementById("resultsModalTitle").innerHTML = "API Key Status";
+    document.getElementById("results-content").innerHTML =  `Your key is valid until <br> ${data.expiry}`;
+    resutlsModal.show()
+}
